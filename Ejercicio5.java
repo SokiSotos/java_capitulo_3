5865475407e2691d2e2101b07e2426be00013f33
SokiSotos <sokisotos@hotmail.com>
2022-10-14 00:28:48 +0200

    Ej5 - Area Rectangulo

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Ejercicio5{
-	public static void main(String args[]){
+	public static void main(String [] args){
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Introduce la base del rectangulo");
 		double base = sc.nextDouble();
