a8063af9b93be03bca23895d772d03b01c1112b9
SokiSotos <sokisotos@hotmail.com>
2022-10-14 00:29:06 +0200

    Ej9 - Volumen Cono

--- Ejercicio9.java
+++ Ejercicio9.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Ejercicio9{
-	public static void main(String args[]){
+	public static void main(String [] args){
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Introduce el radio del cono");
 		double radio = sc.nextDouble();
