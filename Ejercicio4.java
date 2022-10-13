5ff55bc360d6c20e881d9c66330c6d6eb17c39d0
SokiSotos <sokisotos@hotmail.com>
2022-10-13 22:41:07 +0200

    Ej4 - Operadores

---
+++ Ejercicio4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Ejercicio4{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce el primer numero");
+		int n1 = sc.nextInt();
+		System.out.println("Introduce el segundo numero");
+		int n2 = sc.nextInt();
+            System.out.println(n1 + " + " + n2 + " = " + (n1+n2));
+            System.out.println(n1 + " - " + n2 + " = " + (n1-n2));
+            System.out.println(n1 + " * " + n2 + " = " + (n1*n2));
+            System.out.printf(n1 + " / " + n2 + " = %.2f",(double)n1/n2);
+    sc.close();
+	}
+}
