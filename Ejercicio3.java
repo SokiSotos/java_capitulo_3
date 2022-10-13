e70111c9f2f5e470930e66f310714da7d66c231a
SokiSotos <sokisotos@hotmail.com>
2022-10-14 00:28:24 +0200

    Ej3 - Conversor Pesetas

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Ejercicio3{
-	public static void main(String args[]){
+	public static void main(String [] args){
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Introduce la cantidad de pesetas que quiere convertir");
 		int pesetas = sc.nextInt();
