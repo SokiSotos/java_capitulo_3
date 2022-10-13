a8fd869133e06fd5f76777c7071297e131190756
SokiSotos <sokisotos@hotmail.com>
2022-10-13 22:21:46 +0200

    Ej2 - Conversor Euros

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -3,9 +3,9 @@ public class Ejercicio2{
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Introduce la cantidad de euros que quiere convertir");
-		int euros = sc.nextInt();
-        double total = (double)euros*166.386;
-    		System.out.printf(euros + " Euros son %.2f Pesetas",total);
+		double euros = sc.nextDouble();
+        double total = euros*166.386;
+            System.out.printf(euros + " Euros son un total de %d Pesetas\n",(int)total);
     sc.close();
 	}
 }
