3c60fe29e7f0aa69c3d7cde501bfc187204ee713
SokiSotos <sokisotos@hotmail.com>
2022-10-14 00:29:22 +0200

    Ej11 - Conversion KBs

--- Ejercicio11.java
+++ Ejercicio11.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Ejercicio11{
-	public static void main(String args[]){
+	public static void main(String [] args){
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Introduce la cantidad de KBs que quieres convertir a MBs");
 		double kb = sc.nextDouble();
